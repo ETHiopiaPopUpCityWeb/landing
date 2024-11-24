cfe1d252c0b92684298788568e07dbbd5ddb9de7
Dawit Mellese <dawitm777@gmail.com>
2024-11-24 15:25:53 +0300

    fix: hero sections typos

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -8,12 +8,13 @@ const Hero = () => {
           <div className="py-8 flex gap-5 max-sm:flex-col max-sm:p-[30px] justify-center items-center">
             <div className="w-1/2 max-sm:w-[100%]">
               <h1 className="text-[52px] max-sm:text-2xl font-semibold  leading-none">
-               Lorem ipsum, dolor sit amet consectetur.
+                Lorem ipsum, dolor sit amet consectetur.
               </h1>
             </div>
             <div className="w-1/2 max-sm:w-full flex items-center justify-center h-full">
-              <h4 className="max-sm:w-full w-[400px] text-sm m-auto">
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quos nihil est, soluta accusantium repudiandae repellendus.
+              <h4 className="max-sm:w-full w-[400px] text-base m-auto">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quos
+                nihil est, soluta accusantium repudiandae repellendus.
               </h4>
             </div>
           </div>
@@ -26,9 +27,7 @@ const Hero = () => {
             <HeroCard number={21000} title="Lorem" />
           </div>
           <div className="flex-1 relative h-[250px]  max-sm:h-[auto]">
-            <div className="h-[120px] w-[120px] max-sm:w-[100px] max-sm:h-[100px] rounded-full absolute right-[5%] max-sm:top-[-25%] top-[-25%] bg-yellow-300">
-              
-            </div>
+            <div className="h-[120px] w-[120px] max-sm:w-[100px] max-sm:h-[100px] rounded-full absolute right-[5%] max-sm:top-[-25%] top-[-25%] bg-yellow-300"></div>
             <img
               src="https://placehold.co/600x400"
               alt="Event"
