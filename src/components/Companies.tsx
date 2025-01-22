795695b8c7299ebce6ceec9d270ccce2608547ba
Dawit Mellese <dawitm777@gmail.com>
2025-01-22 17:01:00 +0300

    feat: Update Companies component to separate sponsors and partners; enhance layout and styling

--- src/components/Companies.tsx
+++ src/components/Companies.tsx
@@ -1,21 +1,58 @@
-const companies = ["/2.png", "/5.jpg", "/Lisk.svg", "/pic1.jpg", "/pic2.jpg"];
+const sponsers = ["/ethereum.png", "/Lisk.svg"];
+const partners = [
+  "/2.png",
+  "/5.jpg",
+  "/pic1.jpg",
+  "/pic2.jpg",
+  "/MGS.jpg",
+  "/nounsdao.png",
+  "/safaridao.png",
+  "/Women Tech Makers.jpg",
+  "/Xerxis.JPG",
+];
 
 const CompanyLogos = () => {
   return (
-    <section className="py-10">
+    <section className="py-16 bg-gray-50">
       <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-        <div className="mb-10 text-center">
-          <span className="text-orange-600 text-center font-medium mb-4 block">
-            OUR PARTNERS
+        {/* Header */}
+        <div className="mb-12 text-center">
+          <span className="text-orange-600 font-medium uppercase tracking-wide">
+            Our Partners
           </span>
-          <h1 className="text-4xl text-sky-900 text-center font-bold">
+          <h1 className="mt-2 text-4xl font-bold text-sky-900">
             We work with the best partners
           </h1>
         </div>
-        <div className="grid grid-cols-1 gap-4 md:grid-cols-3  xl:grid-cols-6">
-          {companies.map((company) => (
-            <div key={company} className="">
-              <img className="w-32 h-20" src={company} alt="" />
+
+        {/* Sponsors */}
+        <div className="grid grid-cols-2 gap-8 md:grid-cols-4 lg:grid-cols-6 items-center justify-center mb-12 w-full mx-auto">
+          {sponsers.map((company) => (
+            <div
+              key={company}
+              className="flex items-center justify-center p-4 bg-white  rounded-lg"
+            >
+              <img
+                className="max-h-20 object-contain"
+                src={company}
+                alt="Sponsor logo"
+              />
+            </div>
+          ))}
+        </div>
+
+        {/* Partners */}
+        <div className="grid grid-cols-2 gap-6 sm:grid-cols-3 lg:grid-cols-9 items-center justify-center w-full mx-auto">
+          {partners.map((company) => (
+            <div
+              key={company}
+              className="flex items-center justify-center p-3 bg-white  rounded-lg"
+            >
+              <img
+                className="max-h-12 object-contain"
+                src={company}
+                alt="Partner logo"
+              />
             </div>
           ))}
         </div>
